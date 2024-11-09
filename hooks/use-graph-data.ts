7dd200ef475f315876e70308fab0c94347ec54df
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2024-11-09 19:44:40 +0700

    fix(history-data): :zap: Remove emitter filter

--- hooks/use-graph-data.ts
+++ hooks/use-graph-data.ts
@@ -1,32 +1,11 @@
 import { useSensorByHour } from "@/services/swr/use-sensor";
-import { Sensor } from "@/types/sensor.type";
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
 const useGraphData = () => {
   const [filter, setFilter] = useState<number>(5);
-  const [emitter, setEmitter] = useState<string>("5-min-sensor-data");
-  const [data, setData] = useState<Sensor[]>([])
-  const {data: graphData, isLoading} = useSensorByHour(filter);
+  const { data: graphData, isLoading, mutate } = useSensorByHour(filter);
 
-  useEffect(() => {
-    if (filter === 5) {
-      setEmitter("5-min-sensor-data");
-    } else if (filter === 15) {
-      setEmitter("15-min-sensor-data");
-    } else if (filter === 30) {
-      setEmitter("30-min-sensor-data");
-    } else if (filter === 60) {
-      setEmitter("60-min-sensor-data");
-    }
-  }, [filter]);
-
-  useEffect(() => {
-    if (graphData) {
-      setData(graphData.data);
-    }
-  }, [graphData]);
-  
   useEffect(() => {
     const socket = io(`${process.env.NEXT_PUBLIC_WEBSOCKET_API}`);
 
@@ -34,8 +13,10 @@ const useGraphData = () => {
       console.log("Connected to WebSocket server");
     });
 
-    socket.on(emitter, (data) => {
-      setData(data);
+    socket.on("update-data", (data) => {
+      if (data.status) {
+        mutate();
+      }
     });
 
     socket.on("disconnect", () => {
@@ -45,12 +26,12 @@ const useGraphData = () => {
     return () => {
       socket.disconnect();
     };
-  }, [filter, emitter]);
+  }, []);
 
   return {
     filter,
     setFilter,
-    data,
+    data: graphData?.data || [],
     isLoading,
   };
 };
